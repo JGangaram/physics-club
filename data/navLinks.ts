e0769caa69a12f5fe8f57a0a6138ac40dd175156
JGangaram <jeremy@gangaram.com>
2023-07-05 16:35:49 -0400

    added navbar

---
+++ data/navLinks.ts
@@ -0,0 +1,7 @@
+const navLinks = [
+    {href: "/", title: "Home"},
+    {href: "/about", title: "About"},
+    {href: "/members", title: "Members"},
+    {href: "/donate", title: "Donate"}
+]
+export default navLinks
