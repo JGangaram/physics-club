54d8a38083aac210114f8a4b6a94351b8356f007
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-07-12 16:47:50 -0400

    make logo clickable, add events nav link

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,7 +1,8 @@
 const navLinks = [
     {href: "/", title: "Home"},
+    {href: "/events", title: "Events"},
     {href: "/about", title: "About"},
     {href: "/members", title: "Members"},
-    {href: "/donate", title: "Donate"}
+    {href: "/donate", title: "Donate"},
 ]
 export default navLinks
