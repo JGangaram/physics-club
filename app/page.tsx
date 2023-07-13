50dee82a99b85740412104e2bd51e4b707292e34
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-07-13 00:05:54 -0400

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
       <div>
-        <h1 className='text-3xl'
-        >Welcome to the Physics Club</h1>
+        <h1 className='text-3xl'>
+          Welcome to the Physics Club
+        </h1>
       </div>
     </main>
   )
