4b35dc1d19979212e63ce8b46952cd842d434d0b
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-07-13 00:04:10 -0400

    add research to navbar

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,7 +1,8 @@
 const navLinks = [
     {href: "/", title: "Home"},
-    {href: "/events", title: "Events"},
     {href: "/about", title: "About"},
+    {href: "/events", title: "Events"},
+    {href: "/research", title: "Research"},
     {href: "/members", title: "Members"},
     {href: "/donate", title: "Donate"},
 ]
